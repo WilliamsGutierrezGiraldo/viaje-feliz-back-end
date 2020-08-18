eaf9f5ee74cc071fe104ab5a0e3938ec8da0d4ec
wagutierrez <wagutierrez@modusoftware.com>
2020-08-18 12:00:16 -0500

    Se agrega cliente rest

---
+++ src/main/java/com/udistrital/viajefeliz/client/ProductServiceRestClient.java
@@ -0,0 +1,66 @@
+package com.udistrital.viajefeliz.client;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+/**
+ * Cliente de prueba para conectar con API's externas
+ * 
+ * @author wagutierrez
+ *
+ */
+@Service
+public class ProductServiceRestClient {
+	
+	private RestTemplate restTemplate;
+	
+	private String url = "http://192.168.90.33:6005/productProperties";
+	
+	public ProductServiceRestClient() {
+		this.restTemplate = new RestTemplateBuilder()
+				.setConnectTimeout(Duration.ofSeconds(30))
+				.setReadTimeout(Duration.ofSeconds(30))
+				.build();
+	}
+	
+	public List<Map<String, Object>> invokeProductProperties(Long userId) {
+		
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(this.url);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(new MediaType[] {MediaType.APPLICATION_JSON}));
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		Map<String, Object> body = new HashMap<>();
+		body.put("userId", userId);
+		body.put("productId", 10);
+		
+		HttpEntity<Map<String, Object>> entity = new HttpEntity<Map<String, Object>>(body, headers);
+		
+		ParameterizedTypeReference<List<Map<String, Object>>> typeRef = 
+				new ParameterizedTypeReference<List<Map<String,Object>>>() {};
+		
+		ResponseEntity<List<Map<String, Object>>> response = 
+				this.restTemplate.exchange(uriBuilder.build().toString(), HttpMethod.POST, entity, typeRef);
+		
+		System.out.print(response.getBody().toString());
+		
+		return response.getBody();
+		
+	}
+
+}
--- src/main/java/com/udistrital/viajefeliz/service/ViviendaService.java
+++ src/main/java/com/udistrital/viajefeliz/service/ViviendaService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.udistrital.viajefeliz.client.ProductServiceRestClient;
 import com.udistrital.viajefeliz.dao.ViviendaDAO;
 import com.udistrital.viajefeliz.dto.ViviendaDTO;
 
@@ -22,12 +23,16 @@ public class ViviendaService {
 	@Autowired
 	private ViviendaDAO viviendaDao;
 	
+	@Autowired
+	private ProductServiceRestClient client;
+	
 	
 	public ViviendaDTO consultarViviendaPorId(Long id) {
 		return viviendaDao.consultarViviendaPorId(id);
 	}
 	
 	public List<ViviendaDTO> consultarViviendas() {
+		client.invokeProductProperties(3777L);
 		return  viviendaDao.consultarViviendas();
 	}
 
